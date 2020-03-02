fd4faf75006771fa51cc6c92f38c380668568b2c
Scott Harlan <seharlan@gmail.com>
2020-03-02 14:33:08 -0800

    board created

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react'
+import { getInitGameState } from './helper.js';
+
 
 export default class GameBoard extends Component {
+    state = { gameboard: getInitGameState()}
+
+    componentDidMount = () => {
+        this.setState({gameboard: getInitGameState()})
+    }
     render() {
+        console.log('gameboard', this.state.gameboard)
+        
+        const rowNodes = getInitGameState().map((rows, i) => {
+            const cellNodes = getInitGameState()[0].map((cell, j) => {
+                return <div className="cell" id={`cell_${i}-${j}`}></div>
+            })
+
+            return (<div className="row" id={`row_${i}`}>{cellNodes}</div>)
+        })
+        console.log(rowNodes)
         return (
             <div>
-                GAMEBOARD
+                <h2>Mosaic</h2>
+                <div id="gamebaord-container">
+                    {rowNodes}
+                </div>
+                <div id="tile-preview">preview</div>
+                <div> back button</div>
+                <div>bottom drawer</div>
                 
             </div>
         )
