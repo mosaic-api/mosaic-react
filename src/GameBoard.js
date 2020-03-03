404461b76a4e8f0984a0483c55d4544a2d62cfb7
joegpdx <joegpdx@gmail.com>
2020-03-02 17:09:19 -0800

    mob colors are on the board

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -3,17 +3,36 @@ import { getInitGameState } from './helper.js';
 
 
 export default class GameBoard extends Component {
-    state = { gameboard: getInitGameState()}
+    state = { 
+        startColor: '#000000', 
+        gameboard: getInitGameState()
+    }
 
     componentDidMount = () => {
-        this.setState({gameboard: getInitGameState()})
+        this.setState({
+            gameboard: getInitGameState(), 
+            startColor: this.props.startColor
+        })
     }
+    handleClick = (e) => {
+        const cellId = e.target.id
+        const idArray = cellId.split('_')[1].split('-');
+        console.log(cellId)
+        const newBoard = this.state.gameboard.slice()
+       
+        newBoard[Number(idArray[0])][Number(idArray[1])] = this.props.startColor
+
+        this.setState({
+            gameboard: newBoard
+        })
+    }
+
     render() {
         console.log('gameboard', this.state.gameboard)
         
         const rowNodes = getInitGameState().map((rows, i) => {
             const cellNodes = getInitGameState()[0].map((cell, j) => {
-                return <div className="cell" id={`cell_${i}-${j}`}></div>
+                return <div className="cell" style={{backgroundColor: this.state.gameboard[i][j]}} id={`cell_${i}-${j}`}></div>
             })
 
             return (<div className="row" id={`row_${i}`}>{cellNodes}</div>)
@@ -22,7 +41,7 @@ export default class GameBoard extends Component {
         return (
             <div>
                 <h2 className="title">Mosaic</h2>
-                <div id="gameboard-container">
+                <div id="gameboard-container" onClick= { this.handleClick }>
                     {rowNodes}
                 </div>
                 <div id="tile-preview">preview</div>
