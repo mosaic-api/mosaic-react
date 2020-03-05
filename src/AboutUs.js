73b87abeb9a3f8de7409a30830d7a72db49d300e
Cody Brown <cody6lefever@gmail.com>
2020-03-04 17:41:58 -0800

    about us

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -1,14 +1,48 @@
 import React, { Component } from 'react'
+import './about.css';
+import MosaicTitle from './MosaicTitle.js'; 
+import Button from '@material-ui/core/Button';
+import { withRouter } from 'react-router-dom'
 
-export default class AboutUs extends Component {
+
+
+export default withRouter (class AboutUs extends Component {
+    handleBack = () => this.props.history.goBack();
+
     render() {
         return (
-            <div>
-                This Foo *pretty pic* about: i like blue
-                That Foo *semi pretty pic* about: love hot pink
-                The Other Foo *ugly pic* about: black like my soul
-                The Fool *smexy pic* about: I hate colors
+            <div id="containerDiv">
+                <div id="upperDiv">
+                    <MosaicTitle />
+                    <Button onClick={this.handleBack} variant="contained" size="small" color="secondary">Back...no really, it takes you back</Button>  
+                </div>
+                
+                <div id="scott">
+                    <h2>Scott</h2>
+                    <img src='https://mosaicartsupply.com/wp-content/uploads/2016/01/pattern-rooster-colored-6001.jpg' alt='Scott'></img>                    
+                    <p>How you Relax: Music</p>
+                    <p>Ideal Vacation: New Zealand...land of the hobbits</p>
+                </div>
+                <div id="chris">
+                    <h2>Chris</h2>
+                    <img src='https://images-na.ssl-images-amazon.com/images/I/61FahidyQUL.jpg' alt='Chris'></img>                   
+                    <p>How you Relax: Death Metal 🤘</p>
+                    <p>Ideal Vacation: Nashville, TN</p>
+                </div>
+                <div id="joe">
+                    <h2>Joe</h2>
+                    <img src='https://images-na.ssl-images-amazon.com/images/I/611Rs4mypJL._SX258_BO1,204,203,200_.jpg' alt='Joe'></img>                
+                    <p>How you Relax: Music...definitely music</p>
+                    <p>Ideal Vacation: Maui</p>
+                </div>
+                <div id="cody">
+                    <h2>Cody</h2>
+                    <img src='https://i.pinimg.com/originals/a7/ec/c6/a7ecc6a6ed849fc96f9912feccc5539e.jpg' alt='Cody'></img>                   
+                    <p>How you Relax: That's my secret </p>
+                    <p>Ideal Vacation: New Zealand North and South Island</p>
+                </div>
+
             </div>
         )
     }
-}
+})
