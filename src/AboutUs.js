56065a3226d9c435e824d1baa4cdd6a7cb5c6950
Cody Brown <cody6lefever@gmail.com>
2020-03-05 15:28:17 -0800

    mosaicly done photos

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -19,25 +19,25 @@ export default withRouter (class AboutUs extends Component {
                 
                 <div id="scott">
                     <h2>Scott</h2>
-                    <img src='https://mosaicartsupply.com/wp-content/uploads/2016/01/pattern-rooster-colored-6001.jpg' alt='Scott'></img>                    
+                    <img src='Scott.png' alt='Scott'></img>                    
                     <p>How you Relax: Music</p>
                     <p>Ideal Vacation: New Zealand...land of the hobbits</p>
                 </div>
                 <div id="chris">
                     <h2>Chris</h2>
-                    <img src='https://images-na.ssl-images-amazon.com/images/I/61FahidyQUL.jpg' alt='Chris'></img>                   
+                    <img src='Chris.png' alt='Chris'></img>                   
                     <p>How you Relax: Death Metal <span role="img" aria-label="rock">🤘</span></p>
                     <p>Ideal Vacation: Nashville, TN</p>
                 </div>
                 <div id="joe">
                     <h2>Joe</h2>
-                    <img src='https://images-na.ssl-images-amazon.com/images/I/611Rs4mypJL._SX258_BO1,204,203,200_.jpg' alt='Joe'></img>                
+                    <img src='Joe.png' alt='Joe'></img>                
                     <p>How you Relax: Music...definitely music</p>
                     <p>Ideal Vacation: Maui</p>
                 </div>
                 <div id="cody">
                     <h2>Cody</h2>
-                    <img src='https://i.pinimg.com/originals/a7/ec/c6/a7ecc6a6ed849fc96f9912feccc5539e.jpg' alt='Cody'></img>                   
+                    <img src='Cody.png' alt='Cody'></img>                   
                     <p>How you Relax: That's my secret </p>
                     <p>Ideal Vacation: New Zealand North and South Island</p>
                 </div>
