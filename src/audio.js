59ab314fef8ab17e872733a2100074f5d6010d4b
cficht <chris.ficht@gmail.com>
2020-03-13 16:39:37 -0700

    audio updates, filtering

--- src/audio.js
+++ src/audio.js
@@ -4,35 +4,39 @@ let oscillatorNodes = [];
 let oscillatorGainNodes = [];
 let muteBool = true;
 const hexArray = [261.63, 293.66, 329.63, 349.23, 392.00, 440.00];
+
 // const hexArray = [523.25, 587.33, 659.25, 698.46, 783.99, 880.00];
 
-// reverb
-let convolverTime = .3;
+// lowpass filter
+const LPFilter = audioCtx.createBiquadFilter();
+LPFilter.type = "lowpass";
+LPFilter.frequency.setValueAtTime(1000, audioCtx.currentTime);
+LPFilter.gain.setValueAtTime(40, audioCtx.currentTime);
+
+// hipass filter
+const HPFilter = audioCtx.createBiquadFilter();
+HPFilter.type = "highpass";
+HPFilter.frequency.setValueAtTime(250, audioCtx.currentTime);
+HPFilter.gain.setValueAtTime(40, audioCtx.currentTime);
+
+let convolverTime = .1;
 let convolverEffect = (function() {
     let convolver = audioCtx.createConvolver(),
         noiseBuffer = audioCtx.createBuffer(2, convolverTime * audioCtx.sampleRate, audioCtx.sampleRate),
         left = noiseBuffer.getChannelData(0),
         right = noiseBuffer.getChannelData(1);
-        convolver.normalize = true;
     for (let i = 0; i < noiseBuffer.length; i++) {
         left[i] = Math.random() * 2 - 1;
         right[i] = Math.random() * 2 - 1;
     }
     convolver.buffer = noiseBuffer;
+    convolver.normalize = true;
     return convolver;
 })();
-
-// lowpass filter
-const biquadFilter = audioCtx.createBiquadFilter();
-biquadFilter.type = "lowpass";
-biquadFilter.frequency.setValueAtTime(1000, audioCtx.currentTime);
-biquadFilter.gain.setValueAtTime(25, audioCtx.currentTime);
 
 // initialize audio on componentDidMount
 const audioStart = () => {
     audioCtx.resume();
-    biquadFilter.connect(convolverEffect);
-    convolverEffect.connect(audioCtx.destination);
 };
 
 // create new oscilatorNode
@@ -43,9 +47,12 @@ export const playAudio = (randomColor) => {
     const addGain = oscillatorGainNodes.push(gainNode);
     oscillatorNodes[addNode - 1].connect(gainNode);
     oscillatorGainNodes[addGain - 1].connect(convolverEffect);
+    convolverEffect.connect(HPFilter);
+    HPFilter.connect(LPFilter)
+    LPFilter.connect(audioCtx.destination);
 
     // set start of note
-    muteBool ? oscillatorGainNodes[addGain - 1].gain.linearRampToValueAtTime(.5, audioCtx.currentTime) : oscillatorGainNodes[addGain - 1].gain.setValueAtTime(0, audioCtx.currentTime);
+    muteBool ? oscillatorGainNodes[addGain - 1].gain.linearRampToValueAtTime(.9, audioCtx.currentTime) : oscillatorGainNodes[addGain - 1].gain.setValueAtTime(0, audioCtx.currentTime);
     oscillatorNodes[addNode - 1].type = 'triangle';
     oscillatorNodes[addNode - 1].start(); 
     oscillatorNodes[addNode - 1].frequency.setValueAtTime(hexArray[randomColor], audioCtx.currentTime);
